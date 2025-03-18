6e9a70b679675cfb06a64e571db0145dbd41da26
Baek Gi Chan <laphayen@gmail.com>
2025-03-18 13:24:22 +0900

    #28 - Feat: 스프링 부트 프로젝트 초기화
    yml으로 확장자 변경.
    Spring web, DevTools, Security, Data JPA, Lombok, MySQL Driver, OAuth2 Client 의존성 추가

---
+++ BE/src/main/java/com/ssafy/boney/BoneyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.boney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class BoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoneyApplication.class, args);
+	}
+
+}
