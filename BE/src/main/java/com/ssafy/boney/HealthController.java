1ad808d6f7e04553b4abc9d07379d313994b2ac0
cchh6462 <cchh6462@naver.com>
2025-03-18 13:09:05 +0000

    feat:백앤드, DB 인프라 설정 완료료

---
+++ BE/src/main/java/com/ssafy/boney/HealthController.java
@@ -0,0 +1,20 @@
+package com.ssafy.boney.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<Map<String, String>> healthCheck() {
+        Map<String, String> status = new HashMap<>();
+        status.put("status", "UP");
+        return ResponseEntity.ok(status);
+    }
+}
