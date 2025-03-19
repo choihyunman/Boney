285ddd8c9020e8707a528c6cde696659a8062afc
Baek Gi Chan <laphayen@gmail.com>
2025-03-20 00:04:14 +0900

    feat: 카카오 로그인 콜백 엔드포인트 추가
    `/login/kakao/callback` 엔드포인트 추가하여 인증 코드 반환
    사용자가 카카오 로그인 후 인증 코드를 받을 수 있도록 처리

--- BE/src/main/java/com/ssafy/boney/domain/user/controller/UserController.java
+++ BE/src/main/java/com/ssafy/boney/domain/user/controller/UserController.java
@@ -3,7 +3,11 @@ package com.ssafy.boney.domain.user.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -15,6 +19,7 @@ public class UserController {
     @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
     private String redirectUri;
 
+    // 로그인 버튼 클릭 -> 카카오 인증 페이지 이동
     @GetMapping("/login/kakao")
     public String kakaoLogin() {
         String kakaoAuthUrl = "https://kauth.kakao.com/oauth/authorize"
@@ -24,5 +29,15 @@ public class UserController {
 
         return kakaoAuthUrl;
     }
+
+    // 인증 완료 후 리다이렉트 처리
+    @GetMapping("/login/kakao/callback")
+    public Map<String, String> kakaoLoginCallback(@RequestParam("code") String code) {
+        Map<String, String> response = new HashMap<>();
+        response.put("code", code);
+
+        return response;
+    }
+
 
 }
