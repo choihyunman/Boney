03035ce88181c5538c55b36c60eebc0f8be95c35
Baek Gi Chan <laphayen@gmail.com>
2025-03-19 23:00:20 +0900

    feat: 카카오 동의 페이지 연동 완료

---
+++ BE/src/main/java/com/ssafy/boney/domain/user/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.ssafy.boney.domain.user.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class UserController {
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    private String redirectUri;
+
+    @GetMapping("/login/kakao")
+    public String kakaoLogin() {
+        String kakaoAuthUrl = "https://kauth.kakao.com/oauth/authorize"
+                + "?client_id=" + clientId
+                + "&redirect_uri=" + redirectUri
+                + "&response_type=code";
+
+        return kakaoAuthUrl;
+    }
+
+}
