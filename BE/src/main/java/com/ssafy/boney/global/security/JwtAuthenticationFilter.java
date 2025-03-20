408043b525d3c30b4282be4d00ba0c37e0307fe2
Baek Gi Chan <laphayen@gmail.com>
2025-03-20 16:34:44 +0900

    #22 - Feat: Spring Security 설정 및 JWT 인증 구현
    `SecurityConfig` 설정 추가: JWT 기반 인증 및 `/api/v1/auth/**` 경로 예외 처리.
    `CustomUserDetailsService` 구현: 사용자 이메일 기반 인증 처리.
    `JwtAuthenticationFilter` 추가: 요청 필터링하여 JWT 검증 및 인증 객체 설정.
    `JwtTokenProvider` 구현: JWT 생성, 파싱 및 검증 기능 추가.

---
+++ BE/src/main/java/com/ssafy/boney/global/security/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.ssafy.boney.global.security;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+
+        // "/api/v1/auth/**" 경로는 필터 적용 제외
+        if (requestURI.startsWith("/api/v1/auth/")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String token = resolveToken(request);
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            Claims claims = jwtTokenProvider.parseToken(token);
+            UserDetails userDetails = User.builder()
+                    .username(claims.get("user_email", String.class))
+                    .password("")
+                    .roles(claims.get("role", String.class))
+                    .build();
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    userDetails, token, userDetails.getAuthorities()
+            );
+
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        return (bearerToken != null && bearerToken.startsWith("Bearer ")) ? bearerToken.substring(7) : null;
+    }
+
+
+}
