0a6034b9cb73d5c25b5c70dc516b3623ee7c58b8
rim.ssafy <rim.ssafy@gmail.com>
2025-03-26 10:20:15 +0900

    #87 - Feat: 퀘스트 아이 목록 조회 구현

---
+++ BE/src/main/java/com/ssafy/boney/domain/quest/service/QuestChildService.java
@@ -0,0 +1,35 @@
+package com.ssafy.boney.domain.quest.service;
+
+import com.ssafy.boney.domain.quest.dto.QuestChildResponseDto;
+import com.ssafy.boney.domain.user.entity.ParentChild;
+import com.ssafy.boney.domain.user.entity.User;
+import com.ssafy.boney.domain.user.repository.ParentChildRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// 자녀 목록 조회
+@Service
+@RequiredArgsConstructor
+public class QuestChildService {
+
+    private final ParentChildRepository parentChildRepository;
+
+    public List<QuestChildResponseDto> getChildrenForQuest(User parent) {
+        List<ParentChild> parentChildList = parentChildRepository.findByParent(parent);
+
+        // 생년월일 오름차순 정렬
+        parentChildList.sort(Comparator.comparing(pc -> pc.getChild().getUserBirth()));
+
+        return parentChildList.stream()
+                .map(pc -> new QuestChildResponseDto(
+                        pc.getChild().getUserId(),
+                        pc.getChild().getUserName()))
+                .collect(Collectors.toList());
+    }
+
+
+}
