d08433c56f7dda3dc5d626a56f6b49f492554543
rim.ssafy <rim.ssafy@gmail.com>
2025-03-27 17:03:00 +0900

    #92 - Feat: (보호자) 퀘스트 삭제 기능 추가

--- BE/src/main/java/com/ssafy/boney/domain/quest/service/QuestDetailService.java
+++ BE/src/main/java/com/ssafy/boney/domain/quest/service/QuestDetailService.java
@@ -2,6 +2,7 @@ package com.ssafy.boney.domain.quest.service;
 
 import com.ssafy.boney.domain.quest.dto.QuestDetailResponse;
 import com.ssafy.boney.domain.quest.entity.Quest;
+import com.ssafy.boney.domain.quest.entity.enums.QuestStatus;
 import com.ssafy.boney.domain.quest.exception.QuestNotFoundException;
 import com.ssafy.boney.domain.quest.repository.QuestRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,7 @@ public class QuestDetailService {
 
     private final QuestRepository questRepository;
 
+    // 퀘스트 상세 보기
     @Transactional(readOnly = true)
     public QuestDetailResponse getQuestDetail(Integer parentId, Integer questId) {
         Quest quest = questRepository.findById(questId)
@@ -32,5 +34,25 @@ public class QuestDetailService {
                 .endDate(quest.getEndDate())
                 .questReward(quest.getQuestReward())
                 .build();
+    }
+
+
+    // 퀘스트 삭제
+    @Transactional
+    public void deleteQuest(Integer parentId, Integer questId) {
+        Quest quest = questRepository.findById(questId)
+                .orElseThrow(() -> new QuestNotFoundException("해당 퀘스트를 찾을 수 없습니다."));
+
+        // 보호자 소유 검증
+        if (!quest.getParentChild().getParent().getUserId().equals(parentId)) {
+            throw new QuestNotFoundException("해당 퀘스트를 찾을 수 없습니다.");
+        }
+
+        // 상태 검증: 진행 중인 퀘스트만 삭제 가능
+        if (!quest.getQuestStatus().equals(QuestStatus.IN_PROGRESS)) {
+            throw new IllegalArgumentException("진행 중인 퀘스트만 삭제할 수 있습니다.");
+        }
+
+        questRepository.delete(quest);
     }
 }
