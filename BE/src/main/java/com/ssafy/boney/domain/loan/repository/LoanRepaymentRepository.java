92181a3b68b9e522b04def61e9913ab970914c98
Baek Gi Chan <laphayen@gmail.com>
2025-03-28 15:44:23 +0900

    #77 - Feat: LoanRepaymentRepository 추가
    대출 상환 내역 관리를 위한 JpaRepository 인터페이스 생성.

---
+++ BE/src/main/java/com/ssafy/boney/domain/loan/repository/LoanRepaymentRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.boney.domain.loan.repository;
+
+import com.ssafy.boney.domain.loan.entity.LoanRepayment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoanRepaymentRepository extends JpaRepository<LoanRepayment, Integer> {
+
+
+}
