e3517a0a20ef3f8b5a655e768b013700c6ccb448
rim.ssafy <rim.ssafy@gmail.com>
2025-04-01 02:30:27 +0900

    #94 - Feat: 신용점수 레포지토리 userId 검색 추가

--- BE/src/main/java/com/ssafy/boney/domain/user/repository/CreditScoreRepository.java
+++ BE/src/main/java/com/ssafy/boney/domain/user/repository/CreditScoreRepository.java
@@ -3,5 +3,8 @@ package com.ssafy.boney.domain.user.repository;
 import com.ssafy.boney.domain.user.entity.CreditScore;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CreditScoreRepository extends JpaRepository<CreditScore, Integer> {
+    Optional<CreditScore> findByUser_UserId(Integer userId);
 }
