1869c709dae73b0a5b28eaf388877733ff16d67a
Baek Gi Chan <laphayen@gmail.com>
2025-04-01 13:20:04 +0900

    #68 - Feat: LoanRepository에 자녀 기준 대출 조회 메서드 추가
    `findByParentChild_Child(User child)` 메서드 추가.
    자녀 기준으로 대출 목록 조회 가능.

--- BE/src/main/java/com/ssafy/boney/domain/loan/repository/LoanRepository.java
+++ BE/src/main/java/com/ssafy/boney/domain/loan/repository/LoanRepository.java
@@ -2,10 +2,12 @@ package com.ssafy.boney.domain.loan.repository;
 
 import com.ssafy.boney.domain.loan.entity.Loan;
 import com.ssafy.boney.domain.user.entity.ParentChild;
+import com.ssafy.boney.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface LoanRepository extends JpaRepository<Loan, Integer> {
     List<Loan> findByParentChild(ParentChild parentChild);
+    List<Loan> findByParentChild_Child(User child);
 }
