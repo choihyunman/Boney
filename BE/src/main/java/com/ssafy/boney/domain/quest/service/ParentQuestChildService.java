e0f08dc8129e02d93b65d6368222c0664aab742b
rim.ssafy <rim.ssafy@gmail.com>
2025-04-01 10:02:48 +0900

    #94 - Chore: (보호자) 기능 구분 주석을 API 명세, postman과 통일

--- BE/src/main/java/com/ssafy/boney/domain/quest/service/ParentQuestChildService.java
+++ BE/src/main/java/com/ssafy/boney/domain/quest/service/ParentQuestChildService.java
@@ -11,7 +11,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-// (보호자 페이지) 아이 목록 조회
+// (보호자 페이지) 퀘스트 아이 목록 조회
 @Service
 @RequiredArgsConstructor
 public class ParentQuestChildService {
