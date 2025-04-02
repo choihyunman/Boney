77c9b4a5c0587ba89d60974064ec2185248b116e
choihyunman <cchh6462@naver.com>
2025-04-02 23:06:11 +0900

    #156 Feat: 전자서명 기능 추가

--- FE/types/custom.d.ts
+++ FE/types/custom.d.ts
@@ -12,3 +12,36 @@ declare module "*.jpeg" {
   const content: any;
   export default content;
 }
+
+declare module "react-native-signature-canvas" {
+  import React from "react";
+  import { WebViewProps } from "react-native-webview";
+
+  interface SignatureCanvasProps {
+    webStyle?: string;
+    onOK?: (signature: string) => void;
+    onBegin?: () => void;
+    onEnd?: () => void;
+    onClear?: () => void;
+    onEmpty?: () => void;
+    descriptionText?: string;
+    clearText?: string;
+    confirmText?: string;
+    autoClear?: boolean;
+    imageType?: string;
+    backgroundColor?: string;
+    penColor?: string;
+    dataURL?: string;
+    style?: object;
+  }
+
+  export default class SignatureCanvas extends React.Component<
+    SignatureCanvasProps & { ref?: React.Ref<any> },
+    any
+  > {
+    readSignature: () => void;
+    clearSignature: () => void;
+    getDataURL: () => void;
+    confirmSignature: () => void;
+  }
+}
