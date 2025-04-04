25db7e01e2a1a46a1993c39bb32c5a97c6674dc1
Baek Gi Chan <laphayen@gmail.com>
2025-04-04 16:40:56 +0900

    #181 - Feat: FdsRepository에 거래 기반 삭제 기능 추가
    deleteAllByTransaction(Transaction transaction) 메서드 정의.
    거래(Transaction) 삭제 시 관련 FDS 데이터도 함께 삭제할 수 있도록 확장.

--- BE/src/main/java/com/ssafy/boney/domain/transaction/repository/FdsRepository.java
+++ BE/src/main/java/com/ssafy/boney/domain/transaction/repository/FdsRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.boney.domain.transaction.repository;
 
 import com.ssafy.boney.domain.account.entity.Account;
 import com.ssafy.boney.domain.transaction.entity.Fds;
+import com.ssafy.boney.domain.transaction.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,8 @@ public interface FdsRepository extends JpaRepository<Fds, Integer> {
 
     // FdsRepository.java
     void deleteAllByAccount(Account account);
+
+    void deleteAllByTransaction(Transaction transaction);
 
 
 }
