9f97bf98d9f5a55a08919384b98d4e9043abe2bc
Baek Gi Chan <laphayen@gmail.com>
2025-04-04 16:39:24 +0900

    #181 - Feat: LoanRepaymentRepository에 삭제 메서드 추가
    특정 Loan 기반 상환 내역 삭제 메서드 추가

--- BE/src/main/java/com/ssafy/boney/domain/loan/repository/LoanRepaymentRepository.java
+++ BE/src/main/java/com/ssafy/boney/domain/loan/repository/LoanRepaymentRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.boney.domain.loan.repository;
 
 import com.ssafy.boney.domain.loan.entity.Loan;
 import com.ssafy.boney.domain.loan.entity.LoanRepayment;
+import com.ssafy.boney.domain.user.entity.ParentChild;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,5 +12,7 @@ public interface LoanRepaymentRepository extends JpaRepository<LoanRepayment, In
     // Loan 엔티티로 상환 내역 조회
     List<LoanRepayment> findByLoan(Loan loan);
 
+    void deleteAllByLoan(Loan loan);
+    void deleteAllByLoan_ParentChild(ParentChild parentChild);
 
 }
