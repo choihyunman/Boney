8a9918c57a0c827f597f233c85a7ce2ebcf8d1d6
Baek Gi Chan <laphayen@gmail.com>
2025-04-04 16:47:58 +0900

    #181 - Feat: NotificationRepository에 사용자 기준 알림 목록 조회 메서드 추가
    findByUserOrderByCreatedAtDesc(User user): 사용자의 알림을 생성일 기준 내림차순으로 조회

--- BE/src/main/java/com/ssafy/boney/domain/notification/repository/NotificationRepository.java
+++ BE/src/main/java/com/ssafy/boney/domain/notification/repository/NotificationRepository.java
@@ -5,8 +5,12 @@ import com.ssafy.boney.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+
+    List<Notification> findByUserOrderByCreatedAtDesc(User user);
 
     void deleteAllByUser(User user);
 
