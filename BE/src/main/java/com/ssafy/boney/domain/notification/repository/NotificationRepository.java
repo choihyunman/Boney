6ca3067170617d8bad4cca44523bf8e28ef762e4
Baek Gi Chan <laphayen@gmail.com>
2025-04-04 16:42:17 +0900

    #181 - Feat: NotificationRepository에 사용자 기반 전체 삭제 메서드 추가
    deleteAllByUser(User user): 특정 사용자와 관련된 모든 알림 삭제.

--- BE/src/main/java/com/ssafy/boney/domain/notification/repository/NotificationRepository.java
+++ BE/src/main/java/com/ssafy/boney/domain/notification/repository/NotificationRepository.java
@@ -1,9 +1,14 @@
 package com.ssafy.boney.domain.notification.repository;
 
 import com.ssafy.boney.domain.notification.entity.Notification;
+import com.ssafy.boney.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+
+    void deleteAllByUser(User user);
+
+
 }
