f0d249bb4243ef7f6a5499276d3fb34f6518f773
Baek Gi Chan <laphayen@gmail.com>
2025-04-04 16:02:37 +0900

    #181 - Feat: FdsRepository에 계좌 기반 FDS 기록 삭제 기능 추가
    deleteAllByAccount(Account account) 메서드 정의.
    회원 탈퇴 시 계좌별 FDS 기록 삭제를 위한 지원.

--- BE/src/main/java/com/ssafy/boney/domain/transaction/repository/FdsRepository.java
+++ BE/src/main/java/com/ssafy/boney/domain/transaction/repository/FdsRepository.java
@@ -1,9 +1,15 @@
 package com.ssafy.boney.domain.transaction.repository;
 
+import com.ssafy.boney.domain.account.entity.Account;
 import com.ssafy.boney.domain.transaction.entity.Fds;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FdsRepository extends JpaRepository<Fds, Integer> {
+
+    // FdsRepository.java
+    void deleteAllByAccount(Account account);
+
+
 }
