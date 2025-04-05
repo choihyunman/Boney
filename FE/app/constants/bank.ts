04c4881dd863879255065677d7537d1af73284b1
choihyunman <cchh6462@naver.com>
2025-04-06 00:55:23 +0900

    #156 - Feat: 용돈지금 및 정기용돈 구현

--- FE/app/constants/bank.ts
+++
@@ -1,9 +0,0 @@
-export const BANK_INFO = {
-  1: "버니은행",
-} as const;
-
-export type BankNumber = keyof typeof BANK_INFO;
-
-export const getBankName = (bankNum: BankNumber) => {
-  return BANK_INFO[bankNum];
-};
