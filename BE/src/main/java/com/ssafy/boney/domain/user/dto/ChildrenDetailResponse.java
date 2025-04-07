79c914e556af4673ce4ff5080fcc6f739497b29c
Daekyue <howeorb@naver.com>
2025-04-07 10:19:17 +0900

    #64 - Fix: 아이 조회에 아이 성별 추가

--- BE/src/main/java/com/ssafy/boney/domain/user/dto/ChildrenDetailResponse.java
+++ BE/src/main/java/com/ssafy/boney/domain/user/dto/ChildrenDetailResponse.java
@@ -10,6 +10,7 @@ import lombok.Getter;
 public class ChildrenDetailResponse {
     private Integer childId;
     private String childName;
+    private String childGender;
     private String childAccountNum; // 자녀 계좌 번호
     private Integer bankNum;        // 해당 계좌의 은행 번호
     private Integer creditScore;
